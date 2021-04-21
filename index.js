6cf928024f9fc1b34a5e4d3ad7b92096a3a1d8ab
ClintFix <clint.fix@gmail.com>
2021-04-21 14:52:05 -0600

    exercise 5 complete

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ["summer", "fall", "winter", "spring"];
+    this.count = -1;
   }
 
   /**
@@ -102,7 +103,8 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    this.count++;
+    return this.seasons[this.count % 4];
   }
 }
 
