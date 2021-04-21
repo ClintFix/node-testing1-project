aa1bb101fd27f53fd2ea55c777d1f351bafee2c3
ClintFix <clint.fix@gmail.com>
2021-04-21 13:39:30 -0600

    Exercise 2 function and tests complete

--- index.test.js
+++ index.test.js
@@ -9,15 +9,26 @@ describe('[Exercise 1] trimProperties', () => {
     expect(actual).toEqual(expected)
   })
   test('[2] returns a copy, leaving the original object intact' , () => {
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
-    const actual = utils.trimProperties(input);
-    expect(actual).not.toEqual(input);
+    const input = { foo: 'foo  ', bar: ' bar ', baz: '  baz' };
+    utils.trimProperties(input);
+    const output = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    expect(input).not.toEqual(output);
   })
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
-  test.todo('[3] returns an object with the properties trimmed')
-  test.todo('[4] the object returned is the exact same one we passed in')
+  test('[3] returns an object with the properties trimmed', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimPropertiesMutation(input)
+    expect(actual).toEqual(expected)
+  })
+  test('[4] the object returned is the exact same one we passed in', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    utils.trimPropertiesMutation(input);
+    const output = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    expect(input).toEqual(output)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
